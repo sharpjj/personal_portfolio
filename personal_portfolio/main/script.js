0014a828860db4c9a2c80292edfe63ec68bab649
Jon Sharp <jjsharp94@gmail.com>
2021-04-30 11:12:33 +0100

    First Revision Complete

---
+++ personal_portfolio/main/script.js
@@ -0,0 +1,50 @@
+document.getElementById('home').addEventListener('mouseover', function(){
+        document.getElementById('home').style.color = '#2F4858';
+        document.getElementById('home').style.backgroundColor = 'white';
+});
+
+document.getElementById('home').addEventListener('mouseout', function(){
+    document.getElementById('home').style.color = 'white';
+    document.getElementById('home').style.backgroundColor = '#2F4858';
+});
+
+document.getElementById('about').addEventListener('mouseover', function(){
+    document.getElementById('about').style.color = '#2F4858';
+    document.getElementById('about').style.backgroundColor = 'white';
+});
+
+document.getElementById('about').addEventListener('mouseout', function(){
+document.getElementById('about').style.color = 'white';
+document.getElementById('about').style.backgroundColor = '#2F4858';
+});
+
+
+document.getElementById('p-projects').addEventListener('mouseover', function(){
+    document.getElementById('p-projects').style.color = '#2F4858';
+    document.getElementById('p-projects').style.backgroundColor = 'white';
+});
+
+document.getElementById('p-projects').addEventListener('mouseout', function(){
+document.getElementById('p-projects').style.color = 'white';
+document.getElementById('p-projects').style.backgroundColor = '#2F4858';
+});
+
+document.getElementById('c-projects').addEventListener('mouseover', function(){
+    document.getElementById('c-projects').style.color = '#2F4858';
+    document.getElementById('c-projects').style.backgroundColor = 'white';
+});
+
+document.getElementById('c-projects').addEventListener('mouseout', function(){
+document.getElementById('c-projects').style.color = 'white';
+document.getElementById('c-projects').style.backgroundColor = '#2F4858';
+});
+
+document.getElementById('contact-me').addEventListener('mouseover', function(){
+    document.getElementById('contact-me').style.color = '#2F4858';
+    document.getElementById('contact-me').style.backgroundColor = 'white';
+});
+
+document.getElementById('contact-me').addEventListener('mouseout', function(){
+document.getElementById('contact-me').style.color = 'white';
+document.getElementById('contact-me').style.backgroundColor = '#2F4858';
+});
